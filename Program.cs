403003e69f94cb57446a720e5d54b6d618d87682
GabrielAlves5 <ecgabriel.alves@gmail.com>
2025-03-17 12:05:38 -0300

    Uma tv em ascii art

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Olha essa TV legal");
+Console.WriteLine("_______________");
+Console.WriteLine("|,----------.  |\\");
+Console.WriteLine("||           |=| |");
+Console.WriteLine("||          || | |");
+Console.WriteLine("||       . _o| | | __");
+Console.WriteLine("|`-----------' |/ /~/");
+Console.WriteLine(" ~~~~~~~~~~~~~~~ / /");
+Console.WriteLine("                 ~~");
